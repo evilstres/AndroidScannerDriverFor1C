c60a22d65763128826661254cb8b1c80417b3219
David <davidbes@mail.ru>
2023-12-19 20:23:54 +0300

    fix problems

--- Android/app/src/main/java/com/ptolkachev/androidscanner/Scanner.kt
+++ Android/app/src/main/java/com/ptolkachev/androidscanner/Scanner.kt
@@ -1,103 +1,80 @@
-package com.ptolkachev.androidscanner;
+package com.ptolkachev.androidscanner
 
-import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.ClipboardManager
+import android.content.ClipboardManager.OnPrimaryClipChangedListener
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 
-public class Scanner implements Runnable, ClipboardManager.OnPrimaryClipChangedListener {
-    // in C/C++ code the function will have name
-    // Java_com_ptolkachev_androidscanner_Scanner_OnBarcodeReceived
-    static native void OnBarcodeReceived(long pObject, String barcode);
+class Scanner(var mActivity: Activity, var mCallObject: Long) : Runnable,
+	OnPrimaryClipChangedListener {
+	var mScanMode = "broadcast"
+	var mExtraData: String? = null
+	var mReceiver: BroadcastReceiver? = null
+	fun show() {
+		mActivity.runOnUiThread(this)
+	}
 
-    Activity mActivity;
-    long mCallObject;
+	override fun run() {
+		System.loadLibrary("com_ptolkachev_AndroidScanner")
+	}
 
-    String mScanMode;
-    String mExtraData;
-    BroadcastReceiver mReceiver;
+	fun start(scanMode: String, actionName: String?, extraData: String?) {
+		if (mReceiver != null) {
+			return
+		}
+		mScanMode = scanMode
+		if (mScanMode == "clipboard") {
+			this.clipboardManager?.addPrimaryClipChangedListener(this)
+		} else {
+			mExtraData = extraData
+			mReceiver = object : BroadcastReceiver() {
+				override fun onReceive(context: Context, intent: Intent?) {
+					if (intent != null && intent.hasExtra(mExtraData)) {
+						val barCode = intent.getStringExtra(mExtraData)
+						if (!barCode.isNullOrEmpty()) {
+							OnBarcodeReceived(mCallObject, barCode)
+						}
+					}
+				}
+			}
+			val filter = IntentFilter(actionName)
+			mActivity.registerReceiver(mReceiver, filter)
+		}
+	}
 
-    public Scanner(Activity activity, long pObject) {
-        mActivity = activity;
-        mCallObject = pObject;
-        mReceiver = null;
-        mScanMode = "broadcast";
-    }
+	fun stop() {
+		if (mReceiver != null) {
+			mActivity.unregisterReceiver(mReceiver)
+			mReceiver = null
+		}
+		if (mScanMode == "clipboard") {
+			this.clipboardManager?.removePrimaryClipChangedListener(this)
+		}
+	}
 
-    public void show()
-    {
-        mActivity.runOnUiThread(this);
-    }
+	override fun onPrimaryClipChanged() {
+		val clipboardManager = clipboardManager
+		if (clipboardManager != null) {
+			val clipData = clipboardManager.primaryClip
+			if (clipData != null) {
+				val item = clipData.getItemAt(0)
+				val barCode = item.text.toString()
+				if (barCode.isNotEmpty()) {
+					OnBarcodeReceived(mCallObject, barCode)
+				}
+			}
+		}
+	}
 
-    @Override
-    public void run() {
-        System.loadLibrary("com_ptolkachev_AndroidScanner");
-    }
+	private val clipboardManager: ClipboardManager?
+		get() = mActivity.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
 
-    public void start(String scanMode, String actionName, String extraData) {
-        if (mReceiver != null) {
-            return;
-        }
-
-        mScanMode = scanMode;
-
-        if (mScanMode.equals("clipboard")) {
-            ClipboardManager clipboardManager = getClipboardManager();
-            if (clipboardManager != null) {
-                clipboardManager.addPrimaryClipChangedListener(this);
-            }
-        } else {
-            mExtraData = extraData;
-
-            mReceiver = new BroadcastReceiver() {
-                @Override
-                public void onReceive(Context context, Intent intent) {
-                    if (intent != null && intent.hasExtra(mExtraData)) {
-                        String barCode = intent.getStringExtra(mExtraData);
-                        if (barCode != null && !barCode.isEmpty()) {
-                            OnBarcodeReceived(mCallObject, barCode);
-                        }
-                    }
-                }
-            };
-
-            IntentFilter filter = new IntentFilter(actionName);
-            mActivity.registerReceiver(mReceiver, filter);
-        }
-    }
-
-    public void stop() {
-        if (mReceiver != null) {
-            mActivity.unregisterReceiver(mReceiver);
-            mReceiver = null;
-        }
-        if (mScanMode.equals("clipboard")) {
-            ClipboardManager clipboardManager = getClipboardManager();
-            if (clipboardManager != null) {
-                clipboardManager.removePrimaryClipChangedListener(this);
-            }
-        }
-    }
-
-    @Override
-    public void onPrimaryClipChanged() {
-        ClipboardManager clipboardManager = getClipboardManager();
-        if (clipboardManager != null) {
-            ClipData clipData = clipboardManager.getPrimaryClip();
-            if (clipData != null) {
-                ClipData.Item item = clipData.getItemAt(0);
-                String barCode = item.getText().toString();
-                if (!barCode.isEmpty()) {
-                    OnBarcodeReceived(mCallObject, barCode);
-                }
-            }
-        }
-    }
-
-    private ClipboardManager getClipboardManager() {
-        return (ClipboardManager) mActivity.getSystemService(Context.CLIPBOARD_SERVICE);
-    }
+	companion object {
+		// in C/C++ code the function will have name
+		// Java_com_ptolkachev_androidscanner_Scanner_OnBarcodeReceived
+		external fun OnBarcodeReceived(pObject: Long, barcode: String?)
+	}
 }
